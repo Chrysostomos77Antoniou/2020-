3fe66b1f701bb0c06f3f6a185d40378422ba03da
Chrysostomos77Antoniou <61386938+Chrysostomos77Antoniou@users.noreply.github.com>
2020-10-14 19:15:44 +0300

    Add files via upload

---
+++ python/ΑΣΚΗΣΗ7.py
@@ -0,0 +1,29 @@
+file_in=open("arxeio.txt","r")
+sum=0;
+sum2=0;
+lexi=0;
+protasi=" "
+file_in=open("arxeio.txt","r")
+sum==len(arxeio.txt.readlines())
+sum2==sum-1
+for i in range(sum2):
+  while(i<sum2-2):
+        if(lexi==3):
+          continue
+          print (protasi)
+          break
+        elif(f[i]==" "):
+             continue
+             protasi=protasi+arxeio.txt[i]
+        
+        elif (arxeio.txt[i]>=A and arxeio.txt[i]<=Z):
+           continue
+           lexi=lexi+1;
+           protasi==protasi+arxeio.txt[i]
+        else:
+             continue
+             lexi==0
+             protasi==" "
+
+
+    
